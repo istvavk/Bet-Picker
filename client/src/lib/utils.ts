738303de0776f32031e90a445ecfe2619410ec80
Antonio Ištvanović <87823353+istvavk@users.noreply.github.com>
2024-08-11 01:55:26 +0200

    feat(client): normalizing team names

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -3,7 +3,6 @@ export function normalizeTeamName(name: string): string {
     console.error('Received undefined or empty team name');
     return '';
   }
-  console.log(`Normalizing team name: ${name}`);
   return name
     .replace(/^\s+|\s+$/g, '')  // Trim leading/trailing whitespace
     .replace(/\s+/g, ' ')  // Replace multiple spaces with a single space
